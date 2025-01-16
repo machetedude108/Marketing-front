e2085f92d72634a18ac04a4be7f01767a1ca42b6
Abdo <chevaletvalentin@gmail.com>
2025-01-16 12:19:15 +0100

    ijzreijz

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,14 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   template: `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" routerLink="/">Marketing Management</a>
+        <a class="navbar-brand" routerLink="/">Email Campaign Manager</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -29,9 +27,6 @@ import { CommonModule } from '@angular/common';
         </div>
       </div>
     </nav>
-    <div class="container mt-4">
-      <router-outlet></router-outlet>
-    </div>
   `,
   styles: [`
     .active {
@@ -39,6 +34,4 @@ import { CommonModule } from '@angular/common';
     }
   `]
 })
-export class AppComponent {
-  title = 'subscriber-frontend';
-}
+export class NavbarComponent {}
